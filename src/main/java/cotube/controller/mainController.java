ddc01a9f7ca43aac04f67c2cd712858e3aedb7bc
Ambition001 <yiming.wu@stonybrook.edu>
2019-03-20 14:18:25 -0400

    Update mainController.java

--- src/main/java/cotube/controller/mainController.java
+++ src/main/java/cotube/controller/mainController.java
@@ -1,4 +1,25 @@
 package cotube.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+
+@Controller
 public class mainController {
+    @RequestMapping(value={"/","home.html"})
+    String home(){
+        return "home";
+    }
+
+    @RequestMapping(value={"profile.html"})
+    String profile(){
+        return "profile";
+    }
+
+    @RequestMapping(value={"login.html"})
+    String login(){
+        return "login";
+    }
+
 }
