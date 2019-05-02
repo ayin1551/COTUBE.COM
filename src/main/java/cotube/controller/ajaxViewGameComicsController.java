594d646f8c320230a1aaafe8337d29a31ee03972
Ambition001 <yiming.wu@stonybrook.edu>
2019-05-02 18:19:08 -0400

    BUG FIX

--- src/main/java/cotube/controller/ajaxViewGameComicsController.java
+++ src/main/java/cotube/controller/ajaxViewGameComicsController.java
@@ -14,6 +14,7 @@ import java.util.Date;
 import cotube.domain.Likes;
 import cotube.services.LikesService;
 import cotube.domain.Comic;
+import cotube.services.AccountService;
 import cotube.services.ComicService;
 import cotube.domain.Comments;
 import cotube.services.CommentsService;
